aaa02ecc95dda75512343a7baef6dc064d45887c
Michel Streblow <stroppi@freakmail.de>
2014-06-15 23:59:35 +0200

    add the list function

--- iPadIphone/iPadIphone/ViewController.h
+++ iPadIphone/iPadIphone/ViewController.h
@@ -11,4 +11,6 @@
 
 @interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
+@property(copy, nonatomic) NSArray *liste;
+
 @end
--- iPadIphone/iPadIphone/ViewController.m
+++ iPadIphone/iPadIphone/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.liste = @[@"Michel", @"Volker", @"Rainer", @"Lucas", @"pegasus studios", @"alicorn"];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [self.liste count];
+    
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    NSString *SimpleIdentifier = @"SimpleIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SimpleIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SimpleIdentifier];
+    }
+    
+    cell.textLabel.text = self.liste[indexPath.row];
+    
+    return cell;
+}
+
 
 @end
